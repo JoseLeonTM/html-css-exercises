6d15a9837f2ba597edd851c2b53561ed21a6cd91
JoseLeonTM <jleon@tekmexico.com>
2017-05-03 17:43:21 -0700

    Finished exercise 73

--- 73/73.js
+++ 73/73.js
@@ -1,20 +1,11 @@
 
-// function direction(){
 var ball = document.getElementById('ball');
 
-// ball.style.animationName='bounce';
-// ball.style.left='10px';
-setInterval(function(){
-    ball.style.animationName='left';
-},2000);
-setTimeout(function(){
-    setInterval(function(){
-        ball.style.animationName='right';
-        // console.log(ball.style.left);
-    },2000);
-},1000);
-    // if(ball.style.left >= screen.width){
-// ball.style.animationName='right';
-    // }
-
-// }
+// setInterval(function(){
+//     ball.style.animationName='left';
+// },2000);
+// setTimeout(function(){
+//     setInterval(function(){
+//         ball.style.animationName='right';
+//     },2000);
+// },1000);
